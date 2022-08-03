1855de73aadb40898d7ecb9125fe948555f9ad08
ericlvaughan <ericlvaughan+git@gmail.com>
2022-08-03 14:52:49 -0600

    Okay the simulation seems to basically be working now however I doubt the usefulness of the graph model at this stage. I think it's better to proto an actual game and can try to do graph model after determining if the game actually has any value

--- chicken_proto.py
+++ chicken_proto.py
@@ -20,11 +20,14 @@ class Node:
     def __init__(self, id, distance, trigger_flag, check_flag, flag_state, forward_nodes, node_type):
         self.id = id
         self.distance = distance
+
+        # We might have multiple trigger_flags in the future, and also need to check conditions
         self.triggerFlag = trigger_flag # Does this node set a flag?
         self.checkFlag = check_flag # Which node does this check the flag of?
-        # We might have multiple trigger_flags in the future, and also need to check conditions
-        self.flagState = flag_state  # Is this definitionally 0 by default
-        self.forwardNodes = forward_nodes # array of forward-connecting nodes. flag_state indexes the appropriate node
+        self.flagState = flag_state  # Is this definitionally False by default
+
+       # array of forward-connecting nodes. flag_state indexes the appropriate node
+        self.forwardNodes = forward_nodes
         self.nodeType = node_type
 
 class MapGraph:
@@ -33,6 +36,18 @@ class MapGraph:
 
     def addNode(self, my_node):
         self.nodes.append(my_node)
+
+    def nextNode(self, cur_node):
+        if cur_node.checkFlag >= 0:
+            # Check the flagState of the node that we depend on
+            # FIXME KLUUUDGE
+            if self.nodes[cur_node.checkFlag].flagState:
+                idx = 0
+            else:
+                idx = 1
+            return cur_node.forwardNodes[idx]
+        # If we don't look at any flags, then we only have one forwardNode
+        return cur_node.forwardNodes[0]
 
 def render(my_graph, my_chickens):
     # This is just going to be bespoke for my test levels at the
@@ -79,7 +94,9 @@ def render(my_graph, my_chickens):
             x = my_graph.nodes[x.forwardNodes[0]]
         print_str += "\n"
     print(print_str)
-
+    node_flags = "\nNode 1: {},  Node 6: {}\n".format(my_graph.nodes[1].flagState, my_graph.nodes[6].flagState)
+    node_flags += "\nNode 3 next: {},  Node 10 next: {}\n\n".format(my_graph.nextNode(my_graph.nodes[3]), my_graph.nextNode(my_graph.nodes[10]))
+    print(node_flags)
     for i in range(0, len(my_chickens)):
         chicken = my_chickens[i]
         node_len = my_graph.nodes[chicken.currentNode].distance
@@ -101,20 +118,20 @@ def main():
     graph = MapGraph()
     chickens = []
 
-    graph.addNode(Node(0, 20, False, -1, 0, [1], "path"))
-    graph.addNode(Node(1, 10, True, -1, 0, [2], "plate"))
-    graph.addNode(Node(2, 50, False, -1, 0, [8], "path"))
-    graph.addNode(Node(3, 30, False, -1, 0, [4, 3], "gate"))
-    graph.addNode(Node(4, 10, False, 1, 0, [5], "path"))
-    graph.addNode(Node(5, 10, False, -1, 0, [6], "path"))
-    graph.addNode(Node(6, 10, True, -1, 0, [7], "plate"))
-    graph.addNode(Node(7, 10, False, -1, 0, [8], "path"))
-    graph.addNode(Node(8, 30, False, -1, 0, [9], "path"))
-    graph.addNode(Node(9, 10, False, -1, 0, [13], "path"))
-    graph.addNode(Node(10, 60, False, -1, 0, [11, 10], "gate"))
-    graph.addNode(Node(11, 10, False, 6, 0, [12], "path"))
-    graph.addNode(Node(12, 10, False, -1, 0, [9], "path"))
-    graph.addNode(Node(13, 10, False, -1, 0, [], "goal"))
+    graph.addNode(Node(0, 20, False, -1, False, [1], "path"))
+    graph.addNode(Node(1, 10, True, -1, False, [2], "plate"))
+    graph.addNode(Node(2, 50, False, -1, False, [8], "path"))
+    graph.addNode(Node(3, 25, False, 1, False, [4, 3], "gate"))
+    graph.addNode(Node(4, 10, False, -1, False, [5], "path"))
+    graph.addNode(Node(5, 10, False, -1, False, [6], "path"))
+    graph.addNode(Node(6, 10, True, -1, False, [7], "plate"))
+    graph.addNode(Node(7, 10, False, -1, False, [8], "path"))
+    graph.addNode(Node(8, 30, False, -1, False, [9], "path"))
+    graph.addNode(Node(9, 10, False, -1, False, [13], "path"))
+    graph.addNode(Node(10, 60, False, 6, False, [11, 10], "gate"))
+    graph.addNode(Node(11, 10, False, -1, False, [12], "path"))
+    graph.addNode(Node(12, 10, False, -1, False, [9], "path"))
+    graph.addNode(Node(13, 10, False, -1, False, [], "goal"))
 
     chickens.append(Chicken(1, 0, 0))
     chickens.append(Chicken(1, 3, 0))
@@ -124,11 +141,11 @@ def main():
         # Clear flags which do not exhibit hysteresis
         for i in range(0, len(graph.nodes)):
             if graph.nodes[i].nodeType != "toggle":
-                graph.nodes[i].flagState = 0
+                graph.nodes[i].flagState = False
         for chicken in chickens:
             # Re-set nodes according to current state
             if graph.nodes[chicken.currentNode].triggerFlag:
-                graph.nodes[chicken.currentNode].flagState = 1
+                graph.nodes[chicken.currentNode].flagState = True
 
         # Send out virtual chickens to evaluate triggers
         clean_flags = False
@@ -136,23 +153,21 @@ def main():
             clean_flags = True
             for chicken in chickens:
                 # Send out virtual chickens until flags and triggers converge
-                nextNodeIdx = graph.nodes[chicken.currentNode].forwardNodes[graph.nodes[chicken.currentNode].flagState]
+                nextNodeIdx = graph.nextNode(graph.nodes[chicken.currentNode])
                 nodeLen = graph.nodes[chicken.currentNode].distance
                 triggerFlag = graph.nodes[nextNodeIdx].triggerFlag
-                if triggerFlag and graph.nodes[nextNodeIdx].flagState == 0 and (chicken.nodeProgress + chicken.speed) >= nodeLen:
+                if triggerFlag and graph.nodes[nextNodeIdx].flagState == False and (chicken.nodeProgress + chicken.speed) >= nodeLen:
                     # Set valid triggers at next node, unset triggers from previous node if no other chickens there. There is an ambiguous condition here to make a decision on I think
-                    graph.nodes[nextNodeIdx].flagState = 1
-                    graph.nodes[chicken.currentNode].flagState = 0
+                    graph.nodes[nextNodeIdx].flagState = True
+                    graph.nodes[chicken.currentNode].flagState = False
                     clean_flags = False
-
-
 
         for i in range(0, 3):
             chickens[i].nodeProgress += chickens[i].speed
-
-            current_node = graph.nodes[chicken.currentNode] # FIXME, assuming node idx = node_id for now, but this will eventually break, I'm sure
+            current_node = graph.nodes[chickens[i].currentNode] # FIXME, assuming node idx = node_id for now, but this will eventually break, I'm sure
             if (chickens[i].nodeProgress >= current_node.distance):
-                chickens[i].currentNode = current_node.forwardNodes[current_node.flagState]
+                chickens[i].currentNode = graph.nextNode(current_node)
+                chickens[i].nodeProgress = 0  # FIXME, how to handle overflow from chicken's speed?
                 end_node = 12
                 if chickens[i].currentNode == end_node:
                     chickens[i].complete = True
@@ -165,8 +180,8 @@ def main():
                 game_over = False
 
         render(graph, chickens)
-        import pdb;pdb.set_trace()
         # TODO wait for user input to see the next game step
+        import pdb;pdb.set_trace()
 
     print("The chickens solved the level!")
 
